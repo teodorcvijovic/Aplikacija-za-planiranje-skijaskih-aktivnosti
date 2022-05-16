6215668a1657fdfb7e99af9e51bd5a947e1b484a
teodor <cvijovicteodor@gmail.com>
2022-05-16 23:33:03 +0200

    Nova inicijalna verzija projekta bez __pycache__

---
+++ faza 5/djangoProject/jahorina/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path
+
+from .views import *
+
+urlpatterns = [
+    path('', index, name='index'),
+
+    # authentication
+    path('login/', loginRequest, name='loginRequest'),
+    path('logout/', logoutRequest, name='logoutRequest'),
+    path('register/', register, name='register'),
+    path('instructors/', instructors, name='instructors'),
+]
