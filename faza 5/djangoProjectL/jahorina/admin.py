e68f5b3481eae5ce89eb72fc4ff077df688ded56
lara <pl190560d@student.etf.bg.ac.rs>
2022-05-30 20:44:46 +0200

    Obrisana stara verzija projekta

--- faza 5/djangoProjectL/jahorina/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *;
-
-# Register your models here.
-admin.site.register(SkiInstructor);
-admin.site.register(Acitivity);
-admin.site.register(SkiTrack);
-admin.site.register(Category);
-
